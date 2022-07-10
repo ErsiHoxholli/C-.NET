3eaf408bacec3b0749ba6626a244d260222a677a
unknown <ersii.hoxholli@gmail.com>
2022-07-10 18:24:41 +0200

    C#.NET

---
+++ Puzzles (Core)/Program.cs
@@ -0,0 +1,112 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static int[] randomArray()
+{
+    Random rnd = new Random();
+    int[] arr = new int[10];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(5, 26);
+        Console.WriteLine(arr[i] + " this is " + i + " index");
+    
+    }
+    return arr;    
+}
+
+static int sum(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+static int min(int[] arr)
+{
+    int min = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+static int max(int[] arr)
+{
+    int max = arr[0];
+    for (int i = 0; i > arr.Length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+static string tossCoin()
+{
+    Random rnd = new Random();
+    Console.WriteLine("Tossing a Coin!\n");
+    int b = rnd.Next(0, 2);
+    if (b == 0)
+    {
+        Console.WriteLine("Heads");
+        return "Heads";
+    }
+    else
+    {
+        Console.WriteLine("Tails");
+        return "Tails";
+    }
+}
+
+static double tossMultipleCoins(int num)
+{
+    int headCount = 0;
+    for (int o = 0; o < num; o++)
+    {
+        string result = tossCoin();
+        if ("Heads".Equals(result))
+        {
+            headCount++;
+        }
+    }
+    return headCount / (double)num;
+}
+
+static List<string> names();
+{
+    List<string> fsList = new List<string>(5);
+    List<string> scList = new List<string>(5);
+    fsList.Add("Todd");
+    fsList.Add("Tiffany");
+    fsList.Add("Charlie");
+    fsList.Add("Geneva");
+    fsList.Add("Sydney");
+
+    for (int i = 0; i < fsList.Count; i++)
+    {
+        if (fsList[i].Length > 5)
+            scList.Add(fsList[i]);
+    }
+    return scList;
+}
+
+static void shufle(List<string> list1)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < list1.Count; i++)
+    {
+        int rand = rnd.Next(0, list1.Count);
+        string temp = list1[i];
+        list1[i] = list1[rand];
+        list1[rand] = temp;
+    }
+}
---
+++ collectionsPractice (Core)/Program.cs
@@ -0,0 +1,89 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+////Three Basic Arrays////
+
+//Create an array to hold integer values 0 through 9//
+
+int[] arrayOfInts = new int[10];
+
+for (int i = 0; i < 10; i++ )
+{
+    arrayOfInts[i]=i;
+    Console.WriteLine(arrayOfInts[i]);
+}
+
+//Create an array of the names "Tim", "Martin", "Nikki", & "Sara"//
+
+string[] arrayOfStrings = new string[4] {"Tim", "Martin", "Nicki", "Sara"};
+
+for (int i = 0; i < arrayOfStrings.Length; i++)
+{
+    Console.WriteLine(arrayOfStrings[i]);
+}
+    
+
+//Create an array of length 10 that alternates between true and false values, starting with true//
+
+bool[] arrayOfBoolean = new bool[10];
+
+for (int i = 0; i < arrayOfBoolean.Length; i++)
+{
+    arrayOfBoolean[i] = true;
+    if (i%2==1)
+    {
+        arrayOfBoolean[i] = false;
+    }
+    Console.WriteLine(arrayOfBoolean[i]);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////List of Flavors////
+
+//Create a list of ice cream flavors that holds at least 5 different flavors//
+
+List<string> icecream = new List<string>();
+
+icecream.Add("Pistachio");
+icecream.Add("Chocolate");
+icecream.Add("Vanilla");
+icecream.Add("Yougurt");
+icecream.Add("Cookie");
+
+//Output the length of this list after building it//
+
+Console.WriteLine(icecream.Count);
+
+//Output the third flavor in the list, then remove this value//
+Console.WriteLine(icecream[2]);
+icecream.RemoveAt(2);
+
+//Output the new length of the list//
+Console.WriteLine(icecream.Count);  
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////User Info Dictionary////
+
+//Create a dictionary that will store both string keys as well as string values//
+
+Dictionary<string, string>  myDictionary = new Dictionary<string, string>();
+Random rnd = new Random();
+
+//Add key/value pairs to this dictionary where://
+//  each key is a name from your names array//
+//  each value is a randomly elected flavor from your flavors list.//
+
+for (int i = 0; i < 4; i++)
+{
+        myDictionary.Add(arrayOfStrings[i], icecream[rnd.Next(0, 4)]);
+
+}
+
+//Loop through the dictionary and print out each user's name and their associated ice cream flavor//
+
+foreach(KeyValuePair<string, string> ele1 in myDictionary)
+{
+    Console.WriteLine(ele1.Key + " - " + ele1.Value);
+}
